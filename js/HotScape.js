958fb6b4c34f4af1c372d8e237a631065e80ecc1
nicoaraandrei <AndreiN95@gmail.com>
2017-02-08 01:55:04 +0200

    Added physics to platform

--- js/HotScape.js
+++ js/HotScape.js
@@ -1,9 +1,16 @@
 var scene, camera, renderer, relativeCameraOffset;
 var clock, player, controls;
 
+var physicsWorld, collisionConfiguration, dispatcher, solver, broadphase;
+var rigidBodies = [];
+var pos = new THREE.Vector3();
+var quat = new THREE.Quaternion();;
+
 function init() {
 	setupThreeJS();
+	setupPhysics();
 	setupWorld();
+
 	animate();
 }
 
@@ -29,7 +36,10 @@ function setupThreeJS() {
 }
 
 function setupWorld() {
-	setupPlatform();
+	
+	pos.set(0, -2.0, 0);
+	quat.setFromAxisAngle(new THREE.Vector3(0, 0, 0),-90 * Math.PI / 180);
+	var platform = createPlatform(40, 1, 40, 0, pos, quat, new THREE.MeshBasicMaterial({ color: 0xff0000}));
 
   	var playerGeometry = new THREE.BoxGeometry (0.75, 2, 0.5); // width, height, depth
 	var playerMaterial = new THREE.MeshBasicMaterial ({
@@ -44,17 +54,62 @@ function setupWorld() {
 	controls.moveSpeed = 2;
 }
 
-function setupPlatform() {
-	var platformGeometry = new THREE.PlaneGeometry (10, 30);
-	var platformMaterial = new THREE.MeshBasicMaterial ({
-		color: 0xff0000
-	});
-	var platform = new THREE.Mesh (platformGeometry, platformMaterial);
+function setupPhysics() {
 
+	collisionConfiguration = new Ammo.btSoftBodyRigidBodyCollisionConfiguration();
+				dispatcher = new Ammo.btCollisionDispatcher( collisionConfiguration );
+				broadphase = new Ammo.btDbvtBroadphase();
+				solver = new Ammo.btSequentialImpulseConstraintSolver();
+				softBodySolver = new Ammo.btDefaultSoftBodySolver();
+				physicsWorld = new Ammo.btSoftRigidDynamicsWorld( dispatcher, broadphase, solver, collisionConfiguration, softBodySolver);
+				physicsWorld.setGravity(new Ammo.btVector3(0, -10, 0));
+				physicsWorld.getWorldInfo().set_m_gravity( new Ammo.btVector3( 0, -10, 0 ) );	
+}
+
+function createPlatform(sx, sy, sz, mass, pos, quat, material) {
+
+	var platform = new THREE.Mesh (new THREE.BoxGeometry(sx, sy, sz, 1, 1, 1), material);
+	platform.position.copy(pos);
+	platform.quaternion.copy(quat);
+
+	var shape = new Ammo.btBoxShape(new Ammo.btVector3(sx*0.5,sy*0.5,sz*0.5));
+	shape.setMargin(0.05);
+	var mass = 0;
+
+<<<<<<< Updated upstream
 	platform.translateY (-2);
 	platform.rotation.x = -90 * Math.PI / 180;
+=======
+	createRigidBody(platform, shape, mass, platform.position, platform.quaternion);
+>>>>>>> Stashed changes
 
 	scene.add (platform);
+}
+
+function createRigidBody(threeObject, physicsShape, mass, pos, quat) {
+	
+	var transform = new Ammo.btTransform();
+	transform.setIdentity();
+	transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+	transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+	var motionState = new Ammo.btDefaultMotionState(transform);
+
+	var localInertia = new Ammo.btVector3(0, 0, 0);
+	physicsShape.calculateLocalInertia(mass, localInertia);
+
+	var rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, physicsShape, localInertia);
+	var body = new Ammo.btRigidBody(rbInfo);
+
+	threeObject.userData.physicsBody = body;
+
+	if (mass>0) {
+		rigidBodies.push(threeObject);
+		body.setActivationState(4);
+	}
+
+	physicsWorld.addRigidBody(body);
+
+	return body;
 }
 
 function draw() {
