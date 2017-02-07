352bbc178da8c49b28a64faea4243dd4e4e1165c
Eduard Țurcanu <edwinisback@yahoo.ro>
2017-02-07 03:37:37 +0200

    Attempt#1 to add lights and shadows - Failed

--- js/HotScape.js
+++ js/HotScape.js
@@ -1,5 +1,5 @@
 var scene, camera, renderer, clock, controls;
-var playerGeometry, playerMaterial, player;
+var player;
 
 function init() {
 	setupThreeJS();
@@ -15,10 +15,11 @@ function setupThreeJS() {
 		0.1, // near
 		50 // far
 	);
-	scene.add(camera);
-	camera.lookAt(scene.position);
+	scene.add (camera);
+	camera.lookAt (scene.position);
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize (window.innerWidth, window.innerHeight);
+
 	clock = new THREE.Clock();
 
 	document.body.appendChild (renderer.domElement);
@@ -27,45 +28,51 @@ function setupThreeJS() {
 function setupWorld() {
 	setupPlatform();
 
-  	playerGeometry = new THREE.BoxGeometry (0.75, 2, 0.5); // width, height, depth
-	playerMaterial = new THREE.MeshBasicMaterial ({
-		color: 0x223355,
-		wireframe: true
+  	var playerGeometry = new THREE.BoxGeometry (0.75, 2, 0.5); // width, height, depth
+	var playerMaterial = new THREE.MeshBasicMaterial ({
+		color: 0x223355
 	});
 	player = new THREE.Mesh (playerGeometry, playerMaterial);
 	scene.add (player);
-	controls = new KeyboardControls(player);
+
+	controls = new KeyboardControls (player);
 	controls.moveSpeed = 2;
 }
 
 function setupPlatform() {
 	var platformGeometry = new THREE.PlaneGeometry (10, 30);
-	var platformMaterial = new THREE.MeshBasicMaterial ({color: "rgb(255, 0, 0)"});
+	var platformMaterial = new THREE.MeshBasicMaterial ({
+		color: 0xff0000
+	});
 	var platform = new THREE.Mesh (platformGeometry, platformMaterial);
+
 	platform.translateY (-2.0);
 	platform.rotation.x = -90 * Math.PI / 180;
+
 	scene.add (platform);
 }
 
 function draw() {
-	renderer.render(scene,camera);
+	renderer.render (scene,camera);
 }
 
 function animate() {
 	draw();
 	update();
-	requestAnimationFrame(animate);
+	requestAnimationFrame (animate);
 }
 
 function update() {
-	controls.update(clock.getDelta());
-	var relativeCameraOffset = new THREE.Vector3(0,0.3,2);
-	var cameraOffset = player.localToWorld(relativeCameraOffset);
+	controls.update (clock.getDelta());
+
+	var relativeCameraOffset = new THREE.Vector3 (0, 0.3, 2);
+	var cameraOffset = player.localToWorld (relativeCameraOffset);
+
 	camera.position.x = cameraOffset.x;
 	camera.position.y = cameraOffset.y;
 	camera.position.z = cameraOffset.z;
 
-	camera.lookAt( player.position);
+	camera.lookAt (player.position);
 }
 
 function changePOV (pers) {
