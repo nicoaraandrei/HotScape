cb19988640748cf7abcccc7b3859b231779d3c76
Eduard Țurcanu <edwinisback@yahoo.ro>
2017-02-08 01:23:14 +0200

    Added 'position' to 'Player'

--- js/Player.js
+++ js/Player.js
@@ -1,15 +1,17 @@
-function Player (geom, mat) {
+function Player (geom, mat, pos) {
 	// public properties
 	this.geometry = new THREE.BoxGeometry (1, 1, 1);
 	this.material = new THREE.MeshBasicMaterial ({
 		color: 0x223355
 	});
-	this.position;
+	this.position = new THREE.Vector3();
 
 	if (typeof geom !== 'undefined')
 		this.geometry = geom;
 	if (typeof mat !== 'undefined')
 		this.material = mat;
+	if (typeof pos !== 'undefined')
+		this.position = pos;
 
 	THREE.Mesh.call (this, this.geometry, this.material);
 }
