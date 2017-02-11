ef941274ebe233a487af6fea333cc1575ca407e9
Eduard Țurcanu <edwinisback@yahoo.ro>
2017-02-11 15:56:00 +0200

    Improved (fixed) 'touched from above' collision

--- js/Controls.js
+++ js/Controls.js
@@ -41,7 +41,7 @@ function Controls (object, options) {
 	this.domElement.addEventListener ('mouseup', this.onMouseUp.bind (this), false);
 
 	this.object.addEventListener ('collision', function (other_object) {
-		if (object.position.y > other_object.position.y) {
+		if (object.position.y - object._physijs.height / 2 + 0.1 >= other_object.position.y + other_object._physijs.height / 2) {
 			console.log (object.name + " touched " + other_object.name + " from above");
 			airborne = false;
 		}
